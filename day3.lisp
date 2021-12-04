adbbf6f7f61adbd9d2534681f44e65d1290676e1
tarquin-the-brave <tomsteavenson@gmail.com>
2021-12-04 13:24:40 +0000

    day3 part 2
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- day3.lisp
+++ day3.lisp
@@ -6,21 +6,25 @@
 
 (defvar *binaries* (uiop:read-file-lines "inputs/day3-1.txt"))
 
-(defun get-gamma (bins)
-  (coerce (mapcar #'most-popular (transpose (mapcar (lambda (l) (coerce l 'list)) bins))) 'string)
+(defun char-grid (strings) "transpose a list of strings to a grid of chars grouped by their index"
+  (transpose (mapcar (lambda (l) (coerce l 'list)) strings))
 )
 
-(defun most-popular (chars)
-  (let ((ones 0) (zeros 0))
-    (loop for c in chars
-      do (cond
-        ((equal c #\1 ) (incf ones))
-        ((equal c #\0 ) (incf zeros))
+(defun get-gamma (bins)
+  (coerce (mapcar #'popular (char-grid bins)) 'string)
+)
+
+(defun popular (chars &optional unpopular)
+  (let ((ones (count #\1 chars)) (zeros (count #\0 chars)))
+    (if unpopular
+      (cond
+        ((> zeros ones) #\1)
+        (t #\0)
       )
-    )
-    (cond
-      ((> ones zeros) #\1)
-      (t #\0)
+      (cond
+        ((> zeros ones) #\0)
+        (t #\1)
+      )
     )
   )
 )
@@ -43,3 +47,24 @@
 (defvar *epsilon* (flip-bits *gamma*))
 
 (print (int-product *gamma* *epsilon*))
+
+; Part 2
+
+(defun popular-at-pos (strings pos &optional unpopular)
+  (popular (elt (char-grid strings) pos) unpopular)
+)
+
+(defun rating (bins &optional co2)
+  (let ((idx 0) (numbers (copy-list bins)))
+    (loop while (< 1 (list-length numbers)) do
+      (let ((popular-char (popular-at-pos numbers idx co2)))
+        (setf numbers (remove-if #'(lambda (num) (not (equal popular-char (elt num idx)))) numbers))
+      )
+      (incf idx)
+    )
+    (first numbers)
+  )
+)
+
+(print (int-product (rating *binaries*) (rating *binaries* t)))
+
