c90a8744a28b8f6f55e06b071ee9fbebbca72d48
tarquin-the-brave <tomsteavenson@gmail.com>
2021-12-18 15:25:01 +0000

    refactor day7 to use fuel functions as parameter to binry search
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- day7.lisp
+++ day7.lisp
@@ -4,50 +4,32 @@
 (defparameter *crabs*
   (mapcar #'parse-integer (str:split "," (first (uiop:read-file-lines "inputs/day7-1.txt")))))
 
+(defun triangle (n)
+  (if (= n 0) 0 (+ n (triangle (1- n)))))
+
+(defun bin-search (f n nprev)
+  (let ((prev (funcall f (1- n))) (val (funcall f n)) (next (funcall f (1+ n))))
+    (cond
+      ((< prev val) (bin-search f (floor n 2) n))
+      ((< next val) (bin-search f (+ n (+ 1 (floor (- nprev n) 2))) n))
+      (t val))))
+;
+; Part 1
+;
 (defun fuel1 (crabs pos)
   (let ((f 0))
   (loop for crab in crabs do (incf f (abs (- pos crab))))
   f))
 
+(print (bin-search (lambda (n) (fuel1 *crabs* n)) 500 1000))
+
+;
+; Part 2
+;
 (defun fuel2 (crabs pos)
   (let ((f 0))
   (loop for crab in crabs do (incf f (triangle (abs (- pos crab)))))
   f))
 
-(defun triangle (n)
-  (if (= n 0) 0 (+ n (triangle (1- n)))))
-
-;
-; Part 1
-;
-(defun bin-search-1 (n nprev)
-  (let ((prev (fuel1 *crabs* (1- n))) (val (fuel1 *crabs* n)) (next (fuel1 *crabs* (1+ n))))
-    (cond
-      ((< prev val) (bin-search-1 (floor n 2) n))
-      ((< next val) (bin-search-1 (+ n (+ 1 (floor (- nprev n) 2))) n))
-      (t val))))
-
-(print (bin-search-1 500 1000))
-
-;
-; Part 2
-;
-(defun bin-search-2 (n nprev)
-  (let ((prev (fuel2 *crabs* (1- n))) (val (fuel2 *crabs* n)) (next (fuel2 *crabs* (1+ n))))
-    (cond
-      ((< prev val) (bin-search-2 (floor n 2) n))
-      ((< next val) (bin-search-2 (+ n (+ 1 (floor (- nprev n) 2))) n))
-      (t val))))
-
-(print (bin-search-2 500 1000))
-
-;; TODO: combine bin-search-{1,2} by taking the fuel function as a
-;; parameter.  This quick attempt below didn't work.
-; (defun bin-search (f n nprev)
-;   (let ((prev (f (1- n))) (val (f n)) (next (f (1+ n))))
-;     (cond
-;       ((< prev val) (bin-search f (floor n 2) n))
-;       ((< next val) (bin-search f (+ n (floor (- nprev n) 2)) n))
-;       (t val))))
-; (print (bin-search (lambda (n) (fuel2 *crabs* n)) 500 1000))
+(print (bin-search (lambda (n) (fuel2 *crabs* n)) 500 1000))
 
