37c0a4d0424f90d7f478f19176da93c374073839
tarquin-the-brave <tomsteavenson@gmail.com>
2021-12-18 18:37:48 +0000

    day10 complete
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- day10.lisp
+++ day10.lisp
@@ -9,14 +9,23 @@
       "inputs/day10-1.txt"
       )))
 
-(defun score-char (c)
+(defun score-corrupt-char (c)
   (cond
     ((eq #\) c) 3)
     ((eq #\] c) 57)
     ((eq #\} c) 1197)
     ((eq #\> c) 25137)))
 
-(defun corrupt-score (line)
+(defun score-completion-char (c)
+  (cond
+    ((eq #\) c) 1)
+    ((eq #\] c) 2)
+    ((eq #\} c) 3)
+    ((eq #\> c) 4)))
+
+(defun walk (line)
+  "walk the line.  If corrupted return offending character.
+   If incomplete return the completion characters (the stack)."
   (let
     ((stack (list)))
     (loop for c in line do
@@ -28,16 +37,31 @@
         (t
           (when
             (not (eq (pop stack) c))
-            (return-from corrupt-score (score-char c))))))))
+            (return-from walk c)))))
+    stack))
 
-;
-; Part 1
-;
-(defun part1 (input)
+(defun completion-score (chars)
   (let ((score 0))
-    (loop for line in input do
-      (let ((s (corrupt-score line)))
-        (when s (incf score s))))
+    (loop for c in chars do
+      (setf score (* 5 score))
+      (incf score (score-completion-char c)))
     score))
 
-(print (part1 *input*))
+(defun middle-score (scores)
+  (let
+    ((n (floor (list-length scores) 2))
+     (sorted (sort scores '<)))
+    (nth n sorted)))
+
+(defun day10 (input)
+  (let ((corrupt-score 0) (comp-scores (list)))
+    (loop for line in input do
+      (let ((w (walk line)))
+        (if
+          (typep w 'list)
+          (push (completion-score w) comp-scores)
+          (incf corrupt-score (score-corrupt-char w)))))
+
+    (list corrupt-score (middle-score comp-scores))))
+
+(print (day10 *input*))
