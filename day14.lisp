49a1ccfebd6d12455fb485bbb02178f98fbeed33
tarquin-the-brave <tomsteavenson@gmail.com>
2021-12-20 23:47:10 +0000

    day14 part 2, WIP
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- day14.lisp
+++ day14.lisp
@@ -2,8 +2,8 @@
 (ql:quickload '("str" "uiop"))
 
 (defparameter *input* (uiop:read-file-lines
-                        ; "inputs/day14-example.txt"
-                        "inputs/day14.txt"
+                        "inputs/day14-example.txt"
+                        ; "inputs/day14.txt"
                         ))
 
 (defparameter *template* (pop *input*))
@@ -41,9 +41,9 @@
           (setf m x))))
     m))
 
-(defun part-x (template rules n)
+(defun part-1 (template rules)
   (let
-    ((polymer (coerce (run-steps template rules n) 'list))
+    ((polymer (coerce (run-steps template rules 10) 'list))
      (char-counts (make-array '(100) :initial-element 0)))
     (loop for p in polymer do (incf (aref char-counts (char-code p))))
     (- (reduce #'max char-counts) (min-non-zero char-counts))))
@@ -51,11 +51,82 @@
 ;
 ; Part 1
 ;
-(print (part-x *template* *rules* 10))
+(print (part-1 *template* *rules*))
 
 ;
 ; Part 2
 ;
 ; Part 1 grows at O(2^n)
 ;
-; (print (part-x *template* *rules* 40))
+; Don't try to calculate the polymer, but calculate the number
+; of each pair that will exist.
+(defun make-pairs (template)
+  (let ((pairs (make-hash-table :test 'equal)))
+    (loop for (a b) on (coerce template 'list)
+          when b
+          do (if (gethash (list a b) pairs)
+               (incf (gethash (list a b) pairs))
+               (setf (gethash (list a b) pairs) 1)))
+    pairs))
+
+(defclass polymer ()
+  ((pairs
+     :initarg :pairs
+     :initform (error "slot `pairs` is required")
+     :accessor pairs)
+   (pstart
+     :initarg :pstart
+     :initform (error "slot `pstart` is required")
+     :accessor pstart)
+   (pend
+     :initarg :pend
+     :initform (error "slot `pend` is required")
+     :accessor pend)))
+
+(defun make-polymer (template)
+  (let ((t-chars (coerce template 'list)))
+    (make-instance
+      'polymer
+      :pairs (make-pairs template)
+      :pstart (char-code (first t-chars))
+      :pend (char-code (car (last t-chars))))))
+
+(defmethod counts ((self polymer))
+  (let ((cs (make-array '(100) :initial-element 0)))
+    (loop for pair
+      being the hash-key
+      using (hash-value c) of (pairs self) do
+        (incf (aref cs (char-code (first pair))) c)
+        (incf (aref cs (char-code (second pair))) c))
+    (incf (aref cs (pstart self)))
+    (incf (aref cs (pend self)))
+    (map 'vector (lambda (x) (floor x 2)) cs)))
+
+(defmethod take-step ((self polymer) rules)
+  (let ((new-pairs (make-hash-table :test 'equal)))
+    (loop for pair
+      being the hash-key
+      using (hash-value c) of (pairs self) do
+        (let ((new-char (gethash (coerce pair 'string) rules)))
+            (if new-char
+              ((if (gethash (list (first pair) new-char) new-pairs)
+                 (incf (gethash (list (first pair) new-char) new-pairs) c)
+                 (setf (gethash (list (first pair) new-char) new-pairs) c))
+               (if (gethash (list new-char (second pair)) new-pairs)
+                 (incf (gethash (list new-char (second pair)) new-pairs) c)
+                 (setf (gethash (list new-char (second pair)) new-pairs) c)))
+              (if (gethash pair new-pairs)
+                (incf (gethash pair new-pairs) c)
+                (setf (gethash pair new-pairs) c)))))
+    (setf (pairs self) new-pairs)))
+
+(defmethod take-steps ((self polymer) rules n)
+  (loop repeat n do
+        (take-step self rules)))
+
+(defmethod part-2 ((self polymer) rules n)
+  (take-steps self rules n)
+  (let ((char-counts (counts self)))
+    (- (reduce #'max char-counts) (min-non-zero char-counts))))
+
+; (print (part-2 (make-polymer *template*) *rules* 10))
